b81a62a92790a68cf771a6372a84ea182dfeb2d3
tquang2410 <justicepencil@gmail.com>
2025-06-16 12:03:39 +0700

    Moving components into Separate Files

---
+++ src/components/Logo.js
@@ -0,0 +1,3 @@
+export default function Logo() {
+    return <h1>🧳Far away🌴</h1>
+}
---
+++ src/components/Stats.js
@@ -0,0 +1,20 @@
+export default function Stats({items}) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        );
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = numItems === 0 ? 0 : Math.round((numPacked / numItems) * 100);
+
+    return <footer className="stats">
+        <em>
+            {percentage === 100
+                ? "You got everything done! 👌🏻"
+                : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`
+            }
+        </em>
+    </footer>
+}
