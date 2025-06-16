00c082f3b9e64271b27cbb681b48bae8648582b9
tquang2410 <justicepencil@gmail.com>
2025-06-16 11:39:29 +0700

    Sorting item using Slice

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {useState} from "react";
 
 export default function App() {
     const [items, setItems] = useState([]);
-    const numItems = items.length;
     function handleAddItems(item) {
         setItems((items)=> [...items, item]);
     }
@@ -63,9 +62,19 @@ export default function App() {
     </form>)
   }
   function PackingList({items, onDeleteItem, onToggleItem}) {
+    const [sortBy, setSortBy] = useState("input");
+    let sortedItems;
+    if (sortBy === "input") sortedItems = items;
+    if (sortBy === "description") sortedItems = items
+        .slice()
+        .sort((a, b) => a.description.localeCompare(b.description));
+    if (sortBy === "packed") sortedItems = items
+        .slice()
+        .sort((a, b)=> Number(a.packed) - Number(b.packed));
     return (
-        <ul className="list">
-      {items.map((item)=>
+        <div className="list">
+        <ul >
+      {sortedItems.map((item)=>
           <Item item={item}
                 key={item.id}
                 onDeleteItem={onDeleteItem}
@@ -73,7 +82,15 @@ export default function App() {
           />
       )}
     </ul>
-    )
+            <div className="actions">
+                <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+            </div>
+        </div>
+    );
   }
   function  Item({item, onDeleteItem, onToggleItem}) {
     return (
